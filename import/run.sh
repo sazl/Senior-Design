b6a6c445964f435615ce0cbc82efe831ea261f52
Sami Zeinelabdin <samiabdin@gmail.com>
2014-10-29 09:21:08 +0000

    Import osm files

--- import/run.sh
+++ import/run.sh
@@ -10,13 +10,15 @@ osmosis --read-xml file="map.osm" \
 echo "\nConverting to SUMO net file\n"
 netconvert --osm-files map.osm.xml --output-file map.net.xml \
     -t typemap.xml \
-    --proj.utm true \
     --ramps.guess --remove-edges.by-vclass hov,taxi,bus,delivery,transport,lightrail,cityrail,rail_slow,rail_fast,motorcycle,bicycle,pedestrian \
     --remove-edges.isolated \
     --geometry.remove --verbose \
-    --junctions.join --roundabouts.guess --tls.guess --tls.join \
+    --junctions.join --roundabouts.guess \
+    --tls.guess --tls.join \
     --no-turnarounds.tls --tls.discard-simple
 
 echo "\nGenerating polygon typemap\n"
 polyconvert --net-file map.net.xml \
-    --osm-files map.osm.xml --type-file typemap.xml -o map.poly.xml
+    --osm-files map.osm.xml \
+    --type-file typemap.xml \
+    -o map.poly.xml
